a16649abbf73ec55cea84f8e5a293328f842fe95
mcmikecreations <mcmikecreations@users.noreply.github.com>
2024-11-19 15:27:12 +0000

    deploy: 28fa5d515f9b2a8487d938d3913b2b56268092b8

--- assets/js/course-api.mjs
+++ assets/js/course-api.mjs
@@ -1,10 +1,74 @@
-const courseApiUrl = 'https://mcmikecreations.github.io/tum_info/api/courses.json';
+const courseBaseUrl = 'https://mcmikecreations.github.io/tum_info';
+const courseApiUrl = `${courseBaseUrl}/api/courses.json`;
+
+const courseLangMappings = {
+  'de': {
+    yLabel: 'note',
+    xLabel: 'leute',
+    labels: [
+      { grade: 8.0, text: ' N fail' },
+      { grade: 7.0, text: ' B pass' },
+      { grade: 6.0, text: ' × Nicht erschienen - nicht ausreichend' },
+      { grade: 4.1, text: ' nicht ausreichend' },
+      { grade: 3.4, text: ' ausreichend' },
+      { grade: 2.4, text: ' befriedigend' },
+      { grade: 1.4, text: ' gut' },
+      { grade: 0.0, text: ' sehr gut' },
+    ]
+  },
+  'en': {
+    yLabel: 'grade',
+    xLabel: 'people',
+    labels: [
+      { grade: 8.0, text: ' N fail' },
+      { grade: 7.0, text: ' B pass' },
+      { grade: 6.0, text: ' × Didn\'t show up - not sufficient' },
+      { grade: 4.1, text: ' not sufficient' },
+      { grade: 3.4, text: ' sufficient' },
+      { grade: 2.4, text: ' satisfactory' },
+      { grade: 1.4, text: ' good' },
+      { grade: 0.0, text: ' very good' },
+    ]
+  },
+  'none': {
+    yLabel: 'grade',
+    xLabel: 'people',
+    labels: [
+      { grade: 8.0, text: 'N' },
+      { grade: 7.0, text: 'B' },
+      { grade: 6.0, text: ' ×' },
+      { grade: 4.1, text: '' },
+      { grade: 3.4, text: '' },
+      { grade: 2.4, text: '' },
+      { grade: 1.4, text: '' },
+      { grade: 0.0, text: '' },
+    ]
+  },
+};
 
 class Grade {
     constructor(data) {
         this.grade = data.grade;
         this.people = data.people;
     }
+
+    /**
+     * Grade value or encoding.
+     * @type {number}
+     * 
+     * @example
+     * Grades 1.0-5.0 are mapped as-is.
+     * Grade 6.0 means the student did not show up (X).
+     * Grade 7.0 means the student passed without a grade (B).
+     * Grade 8.0 means the student failed without a grade (N).
+     * As of now there is no encoding for (Q).
+     */
+    grade;
+    /**
+     * Number of people who got this grade.
+     * @type {number}
+     */
+    people;
 }
 
 class Course {
@@ -93,7 +157,9 @@ async function fetchCourse(code, semester, apiUrl) {
 }
 
 export {
+    courseBaseUrl,
     courseApiUrl,
+    courseLangMappings,
     Grade,
     Course,
     fetchCourse,
